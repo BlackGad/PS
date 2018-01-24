68b69799b56410553265d1a16f11a62fe759429b
Vladimir Shkolka <ztBlackGad@gmail.com>
2018-01-24 11:36:35 +0200

    Added missed nuget package definition for ps.predicate.json package

---
+++ PS.Predicate.Json/Nuget.cs
@@ -0,0 +1,10 @@
+﻿using PS.Build.Nuget.Attributes;
+
+[assembly: Nuget(ProjectUrl = "https://github.com/BlackGad/PS")]
+[assembly: Nuget(LicenseUrl = "https://github.com/BlackGad/PS/blob/master/LICENSE")]
+[assembly: NugetFilesFromTarget(IncludePDB = true)]
+[assembly: NugetPackageDependenciesFromConfiguration]
+[assembly: NugetPackageDependenciesFilter("PS.Build*")]
+[assembly: NugetBuild(@"{dir.solution}_Artifacts\{prop.configuration}.{prop.platform}")]
+[assembly: NugetDebugSubstitution]
+[assembly: Nuget(Tags = "PS")]
